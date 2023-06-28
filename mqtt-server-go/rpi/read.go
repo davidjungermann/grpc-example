1fa97c3dd34a8a6fb4b611a8ecd5938616280009
David Jungermann <david.jungermann@gmail.com>
2023-06-28 11:20:42 +0200

    Retrieves device id

--- mqtt-server-go/rpi/read.go
+++ mqtt-server-go/rpi/read.go
@@ -3,26 +3,39 @@ package rpi
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"strconv"
+	"strings"
 )
 
 const cpuTempFile = "/sys/class/thermal/thermal_zone0/temp"
 
 func ReadCPUTemp() (float64, error) {
-	bytes, err := os.ReadFile(cpuTempFile)
-	if err != nil {
-		return 0, fmt.Errorf("could not read temp file: %w", err)
-	}
+    bytes, err := os.ReadFile(cpuTempFile)
+    if err != nil {
+        return 0, fmt.Errorf("could not read temp file: %w", err)
+    }
 
-	// The temperature is represented as a string, in millidegrees Celsius.
-	tempStr := string(bytes[:len(bytes)-1]) // Strip trailing newline
-	tempInt, err := strconv.ParseInt(tempStr, 10, 64)
-	if err != nil {
-		return 0, fmt.Errorf("could not parse temp: %w", err)
-	}
+    // The temperature is represented as a string, in millidegrees Celsius.
+    tempStr := string(bytes[:len(bytes)-1]) // Strip trailing newline
+    tempInt, err := strconv.ParseInt(tempStr, 10, 64)
+    if err != nil {
+        return 0, fmt.Errorf("could not parse temp: %w", err)
+    }
 
-	// Convert millidegrees to degrees.
-	temp := float64(tempInt) / 1000
+    // Convert millidegrees to degrees.
+    temp := float64(tempInt) / 1000
 
-	return temp, nil
+    return temp, nil
+}
+
+func GetDeviceID() (string, error) {
+    out, err := exec.Command("sh", "-c", "cat /proc/cpuinfo | grep Serial | cut -d ' ' -f 2").Output()
+
+    if err != nil {
+        return "", fmt.Errorf("error retrieving Device ID: %w", err)
+    }
+
+    deviceId := strings.TrimSpace(string(out))
+    return deviceId, nil
 }
